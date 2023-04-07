7c82eb02158448ce68f32991ad4004ecd0e75797
fedorkray <130180153+fedorkray@users.noreply.github.com>
2023-04-08 00:13:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,10 +6,6 @@ from scipy.stats import t
 chat_id = 217552503 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    n = len(x)
-    t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
-    x_mean = x.mean()
-    s = np.sqrt(np.var(x))
-    interval = (x_mean - t_alpha_2 * s / np.sqrt(n), x_mean + t_alpha_2 * s / np.sqrt(n))
-    return interval
+    t = 68
+    return 2*(-1/2 - (-x).min() )/(t*t), \
+           2*(-np.log(1-p)/(x.size) - 1/2 - (-x).min() )/(t*t)
