5a305b805432f0ca5af7b4088c6c12716273208b
fedorkray <130180153+fedorkray@users.noreply.github.com>
2023-04-07 23:58:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import t
 
-from scipy.stats import norm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 217552503 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    mu = 2 * x.mean() / 68**2  # среднее значение ускорения
-    sigma = np.sqrt(1/12)  # стандартное отклонение ошибки измерения пути
     alpha = 1 - p
-    z = norm.ppf(1 - alpha/2)
-    h = z * sigma / np.sqrt(n)  # половина длины доверительного интервала
-    return (mu - h, mu + h)
+    n = len(x)
+    t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
+    x_mean = x.mean()
+    s = np.sqrt(np.var(x))
+    interval = (x_mean - t_alpha_2 * s / np.sqrt(n), x_mean + t_alpha_2 * s / np.sqrt(n))
+    return interval
